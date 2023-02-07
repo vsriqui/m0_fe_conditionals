a2f306152beca45cc8a541685140025beec1182d
Victor Sriqui <victorsriqui@gmail.com>
2023-02-07 16:42:16 -0700

    Add mildchlng

---
+++ codeChallenges/mild.js
@@ -0,0 +1,26 @@
+var number = 22
+
+if (number % 2 === 0) {
+    console.log('Even ');
+
+} else  {
+    console.log('Odd ');
+}
+
+var number = 6
+
+if (number % 2 === 0) {
+    console.log('Even ');
+
+} else  {
+    console.log('Odd ');
+}
+
+var number = 71
+
+if (number % 2 === 0) {
+    console.log('Even ');
+
+} else  {
+    console.log('Odd ');
+}
