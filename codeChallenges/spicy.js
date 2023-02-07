5d51f5a280dc8a03b187cae61d7ff7683dc0280f
Victor Sriqui <victorsriqui@gmail.com>
2023-02-07 06:42:06 -0700

    Add spice

---
+++ codeChallenges/spicy.js
@@ -0,0 +1,19 @@
+// # Spicy
+
+var number = Math.floor(Math.random() * 100) + 1;
+console.log(number)
+
+    if (number % 15 === 0) {
+    console.log('FizzBuzz ');
+
+} else if (number % 3 === 0) {
+    console.log('Fizz ');
+
+} else if (number % 5 === 0) {
+    console.log('Buzz ');
+} 
+
+
+   
+
+  
